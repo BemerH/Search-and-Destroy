46820357aa456c6395c327e921b0f1bf3f8ef20a
lucas <lucasgamacavalcante@yahoo.com>
2021-08-14 07:30:10 -0300

    DONE

--- movendo_ponto.py
+++ movendo_ponto.py
@@ -1,22 +1,16 @@
 import random
 
-def arenaJogo(aux,campo):
-    for i in range(aux):
-        for j in range(aux):
-            print(campo[i][j], end='')
-            if j != aux-1:
-                print(' - ',end='')
-        print()
-
 def funControle(move,campo):
     linha = 0
     coluna = 0
     
     aux = 5
     
+    simbol = 'X'
+    
     for i in range(aux):
         for j in range(aux):
-            if campo[i][j] == 'X':
+            if campo[i][j] == simbol:
                 linha = i
                 coluna = j
                 
@@ -31,6 +25,7 @@ def funControle(move,campo):
     elif move == 'D':
         funRight(move,linha,coluna,campo)
         
+"-------------------------------------------------"    
 def funUp(move,linha,coluna,campo):
     campo[linha-1][coluna] = 'X'
 def funDown(move,linha,coluna,campo):
@@ -45,6 +40,7 @@ def funRight(move,linha,coluna,campo):
         campo[linha][0] = 'X'
     else:
         campo[linha][coluna+1] = 'X'
+"-------------------------------------------------"
 
 linha = random.randint(0,4)
 coluna = random.randint(0,4)
@@ -53,6 +49,7 @@ linhaDaBola = random.randint(0,4)
 colunaDaBola = random.randint(0,4) 
 
 campo = [['','','','',''],['','','','',''],['','','','',''],['','','','',''],['','','','','']] 
+
 campo[linha][coluna] = 'X'
 campo[linhaDaBola][colunaDaBola] = 'O'
 
@@ -60,42 +57,33 @@ aux = 5
 
 laco = None
 while True:
-    #arenaJogo(aux,campo)
+    
     temBolinha = False
     for i in range(aux):
         for j in range(aux):
             if campo[i][j] == 'O':
                 temBolinha = True
+
     if temBolinha == False:
-        linhaDaBola = random.randint(0,4)
-        colunaDaBola = random.randint(0,4)   
-        campo[linhaDaBola][colunaDaBola] = 'O'
-    """
-    for i in range(aux):
-        for i in range(aux):
-            if(campo[linha][coluna] != 'O'):
-                while True:
-                    linhaDaBola = random.randint(0,4)
-                    colunaDaBola = random.randint(0,4)
-                    
-                    if linha != linhaDaBola or coluna != colunaDaBola:
-                        campo[linhaDaBola][colunaDaBola] = 'O'   
-                        break
-      """  
+        while True:
+            linhaDaBola = random.randint(0,4)
+            colunaDaBola = random.randint(0,4) 
+            if campo[linhaDaBola][colunaDaBola] != 'X':
+                campo[linhaDaBola][colunaDaBola] = 'O'
+                break
+    
     for i in range(aux):
         for j in range(aux):
-                
             print(campo[i][j], end='')
             if j != aux-1:
                 print(' - ',end='')
         print()
     
-    move = input('Type W,A,S,D:')
+    move = input('Type W,A,S,D to move the X and catch the ball. Type 0 if u wanna leave!\n')
     
     if move == '0':
         break
     elif move == 'W' or move == 'A' or move == 'S' or move == 'D':
         funControle(move,campo)
-        
     else:
         print('\nAre u kidding me? Type correctly please!\n')
